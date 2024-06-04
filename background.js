e02cc8d5537a2c4fcdb1f6d8f0c765692796fe88
IshticBlue <156532026+Ishimistic@users.noreply.github.com>
2024-06-04 14:26:14 +0530

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-chrome.webRequest.onBeforeRequest.addListener(
-    function(detals) {return  {cancel : true}},
-    {urls : ["*://*.netflix.com/*", "*://*.fmoviesz.to/*"]},
-    ["blocking"]
-)
